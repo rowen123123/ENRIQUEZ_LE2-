ae26237ceaa8e8303926aab679d070baff082a38
rowen123123 <rowenneil2468@gmail.com>
2025-05-16 19:32:26 +0800

    Initial commit - Added BlogDataLibrary, BlogTestUI, and BlogDB

---
+++ BlogDataLibrary/Data/SqlData.cs
@@ -0,0 +1,14 @@
+﻿using BlogDataLibrary.Database;
+
+public class SqlData
+{
+    private  ISqlDataAccess _db;
+    private const string connectionStringName = "SqlDb";
+
+    public SqlData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+}
+
